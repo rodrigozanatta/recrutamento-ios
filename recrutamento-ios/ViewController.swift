11f63c559177deb7287261a77044207f5d13ca0c
rodrigozanattasilva <rodrigozanattasilva@gmail.com>
2015-09-10 15:15:05 -0300

    Primeiro comit para adicionar os arquivos do projeto e testar que o git esta funcionando corretamente.

---
+++ recrutamento-ios/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  recrutamento-ios
+//
+//  Created by Rodrigo Zanatta Silva on 10/09/15.
+//  Copyright (c) 2015 Rodrigo Zanatta Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //testando github para começar!!!
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
