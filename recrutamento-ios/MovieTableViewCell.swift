394ac694f5a078381cdf3d7b9ac2871b449c73be
rodrigozanattasilva <rodrigozanattasilva@gmail.com>
2015-09-10 16:09:54 -0300

    Terminei de criar o storyboard com o autolayout funcionando. Deu um pequeno trabalho.

---
+++ recrutamento-ios/MovieTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieTableViewCell.swift
+//  recrutamento-ios
+//
+//  Created by Rodrigo Zanatta Silva on 10/09/15.
+//  Copyright (c) 2015 Rodrigo Zanatta Silva. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var leftImage: UIImageView!
+    @IBOutlet weak var centerImage: UIImageView!
+    @IBOutlet weak var rightImage: UIImageView!
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var rightLabel: UIView!
+
+}
